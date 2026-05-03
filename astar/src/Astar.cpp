de08b4b534c17b2ee22013b93d4271ba97a1f641
kendrick <kendrick@kendrickdeMacBook-Air.local>
2026-05-03 15:39:54 +0800

    astar pass

--- astar/src/Astar.cpp
+++ astar/src/Astar.cpp
@@ -35,7 +35,7 @@ bool readScene(const string& path, Scene& scene) {
     fin >> scene.start.x >> scene.start.y >> scene.start.z;
     fin >> scene.goal.x >> scene.goal.y >> scene.goal.z;
     fin >> K;
-    scene.obstacles.resize(K);
+    scene.obstacles.resize(K);  // K个障碍物
     for (int i = 0; i < K; ++i) {
         fin >> scene.obstacles[i].x1 >> scene.obstacles[i].y1 >> scene.obstacles[i].z1
             >> scene.obstacles[i].x2 >> scene.obstacles[i].y2 >> scene.obstacles[i].z2
@@ -44,7 +44,7 @@ bool readScene(const string& path, Scene& scene) {
     return true;
 }
 
-vector<vector<vector<int>>> buildOccupancy(const Scene& scene) {
+vector<vector<vector<int>>> buildOccupancy(const Scene& scene) { //返回三维动态数组
     vector<vector<vector<int>>> occ(scene.X, vector<vector<int>>(scene.Y, vector<int>(scene.Z, 0)));
     for (const auto& o : scene.obstacles) {
         for (int x = max(0, o.x1); x <= min(scene.X - 1, o.x2); ++x) {
@@ -58,32 +58,42 @@ vector<vector<vector<int>>> buildOccupancy(const Scene& scene) {
     return occ;
 }
 
-bool inside(const Scene& scene, const Point& p) {
+bool inside(const Scene& scene, const Point& p) { // 判断点p是否在场景内
     return p.x >= 0 && p.x < scene.X && p.y >= 0 && p.y < scene.Y && p.z >= 0 && p.z < scene.Z;
 }
 
-bool freeCell(const Scene& scene, const vector<vector<vector<int>>>& occ, const Point& p) {
-    return inside(scene, p) && occ[p.x][p.y][p.z] == 0;
+bool freeCell(const Scene& scene, const vector<vector<vector<int>>>& occ, const Point& p) { //碰撞检测
+    return inside(scene, p) && occ[p.x][p.y][p.z] == 0; 
 }
 
-int flattenId(const Scene& scene, const Point& p) {
+int flattenId(const Scene& scene, const Point& p) { // 将三维坐标转换为一维id
     return (p.x * scene.Y + p.y) * scene.Z + p.z;
 }
 
-Point pointFromId(const Scene& scene, int id) {
+Point pointFromId(const Scene& scene, int id) { // 将一维id转换为三维坐标
     return {id / (scene.Y * scene.Z), (id / scene.Z) % scene.Y, id % scene.Z};
 }
 
 int manhattan(const Point& a, const Point& b) {
     // TODO 1: 实现3D曼哈顿距离启发式函数。
     // 提示: |x1-x2| + |y1-y2| + |z1-z2|
-    return 0;
+    int dx = abs(a.x - b.x);
+    int dy = abs(a.y - b.y);
+    int dz = abs(a.z - b.z);
+    return dx + dy + dz;
 }
 
 vector<Point> getNeighbors6(const Point& p) {
     // TODO 2: 生成3D 6邻域。
     // 提示: 每次只改变x/y/z中的一个坐标，变化量为+1或-1。
-    return {};
+    return{
+        {p.x + 1, p.y, p.z},
+        {p.x - 1, p.y, p.z},
+        {p.x, p.y + 1, p.z},
+        {p.x, p.y - 1, p.z},
+        {p.x, p.y, p.z + 1},
+        {p.x, p.y, p.z - 1}
+    };
 }
 
 vector<Point> reconstructPath(const Scene& scene, const vector<int>& parent) {
@@ -117,8 +127,47 @@ vector<Point> astar(const Scene& scene) {
     const int total = scene.X * scene.Y * scene.Z;
     vector<int> g(total, numeric_limits<int>::max());
     vector<int> parent(total, -1);
-    (void)g;
-    (void)parent;
+    struct Node {
+        int f;
+        int id;
+        // 小根堆
+        bool operator<(const Node& other) const {
+            return f > other.f;
+        }
+    };
+    priority_queue<Node> pq; // 小根堆
+    int  start_id = flattenId(scene, scene.start);
+    g[start_id] = 0;
+    pq.push({manhattan(scene.start, scene.goal), start_id});
+    while(!pq.empty()) {
+        Node curr_node = pq.top();
+        pq.pop();
+        Point curr = pointFromId(scene, curr_node.id); 
+
+        if(curr == scene.goal) {    // 到达终点
+            return reconstructPath(scene, parent);
+        }
+
+        if(curr_node.f>g[curr_node.id]+manhattan(curr, scene.goal)) { // 已经有更优的路径
+            continue;
+        }
+
+        //扩展邻居节点
+        for(const Point& nxt : getNeighbors6(curr)){
+            if(freeCell(scene, occ, nxt)) {
+                int nxt_id = flattenId(scene, nxt);
+                int tentative_g = g[curr_node.id]+1;
+                if(tentative_g < g[nxt_id]) { // 找到更优的路径
+                    g[nxt_id] = tentative_g;
+                    parent[nxt_id] = curr_node.id;
+                    int f = tentative_g + manhattan(nxt, scene.goal);
+                    pq.push({f, nxt_id});
+                }
+            }
+                
+        }
+
+    }
 
     return {};
 }
